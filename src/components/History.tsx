dd24c751b307659948697fb2f0a74d703ed33921
lq0-github <1441665200@qq.com>
2024-10-09 17:26:59 +0800

    Fix history time

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -31,6 +31,19 @@ export default function History() {
     setCurrentPage(data.selected);
   };
 
+  function formatTimestamp(timestamp: number) {
+    const date = new Date(timestamp * 1000);
+  
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   // console.log(historyData, "historyData");
 
   return (
@@ -116,8 +129,8 @@ export default function History() {
                       </div>
                     ))}
                   </td>
-                  <td>{formatTimestamp(l.createdAt)}</td>
-                  <td>{formatTimestamp(l.updatedAt)}</td>
+                 <td>{formatTimestamp(l.createdAt)}</td>
+        <td>{formatTimestamp(l.updatedAt)}</td>
                 </tr>
               );
             })}
